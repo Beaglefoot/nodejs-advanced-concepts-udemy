4a62af8a5350c87b2e129963abfc6e17e25af80a
Beaglefoot <ch-e-st@yandex.ru>
2018-09-29 20:58:38 +0300

    Basic test involving OAuth flow

--- tests/header.test.js
+++ tests/header.test.js
@@ -27,3 +27,33 @@ test('clicking login start oauth flow', async () => {
 
   expect(url).toMatch(/accounts\.google\.com/);
 });
+
+test('When signed in, shows logout button', async () => {
+  const { USER_OID } = require('./constants');
+
+  const { Buffer } = require('safe-buffer');
+  const sessionObject = {
+    passport: {
+      user: USER_OID
+    }
+  };
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
+    'base64'
+  );
+
+  const Keygrip = require('keygrip');
+  const keys = require('../config/keys');
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign('session=' + sessionString);
+
+  await page.setCookie({ name: 'session', value: sessionString });
+  await page.setCookie({ name: 'session.sig', value: sig });
+  await page.goto('localhost:3000');
+  // This one looks more correct istead of solution in course
+  // await page.waitFor('a[href="/auth/logout"]');
+  await page.waitForResponse('http://localhost:3000/api/current_user');
+
+  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+  expect(text).toEqual('Logout');
+});
