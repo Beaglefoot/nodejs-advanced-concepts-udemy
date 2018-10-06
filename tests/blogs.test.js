e411fe99ad989241829919026a99688fd8a0e68e
Beaglefoot <ch-e-st@yandex.ru>
2018-10-06 14:38:53 +0300

    Test error messages

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -1,6 +1,7 @@
 const Page = require('./helpers/page');
 
 let page;
+const buttonSelector = 'a[href="/blogs/new"][class^="btn"]';
 
 beforeEach(async () => {
   page = await Page.build();
@@ -11,11 +12,29 @@ afterEach(async () => {
   await page.close();
 });
 
-test('Blog creation form is visible when logged in', async () => {
-  await page.login();
-  await page.click('a[href="/blogs/new"][class^="btn"');
+describe('When logged in', async () => {
+  beforeEach(async () => {
+    await page.login();
+    await page.click(buttonSelector);
+  });
 
-  const label = await page.getContentsOf('form label');
+  test('blog creation form is visible', async () => {
+    const label = await page.getContentsOf('form label');
 
-  expect(label).toEqual('Blog Title');
+    expect(label).toEqual('Blog Title');
+  });
+
+  describe('And using invalid inputs', async () => {
+    beforeEach(async () => {
+      await page.click('form button[type="submit"]');
+    });
+
+    test('the form shows an error message', async () => {
+      const titleError = await page.getContentsOf('.title .red-text');
+      const contentError = await page.getContentsOf('.content .red-text');
+
+      expect(titleError).toEqual('You must provide a value');
+      expect(contentError).toEqual('You must provide a value');
+    });
+  });
 });
