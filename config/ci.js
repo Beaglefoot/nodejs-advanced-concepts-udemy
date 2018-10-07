a9e7e363dce4152a77f78de01fc52c73968a96dc
Beaglefoot <ch-e-st@yandex.ru>
2018-10-07 18:36:08 +0300

    Set up Travis CI

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '144619871289-agequdechtsei825b2i2k2e0bi49t5tn.apps.googleusercontent.com',
+  googleClientSecret: 'RilUMGlO2BSaxya5KsMxxhR0',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
+};
